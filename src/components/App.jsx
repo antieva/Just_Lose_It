db71972c676e69cf214729ba3ed2f5c860799125
Eva Antipina <eva.antipina@gmail.com>
2018-07-20 14:03:04 -0700

    Add style tp login page.

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,37 @@
 import React from 'react';
 import { Switch, Route } from 'react-router-dom';
-import { Button } from 'reactstrap';
+//import { Button } from 'reactstrap';
+import './App.css';
 
 function App(){
   return (
     <div>
-      <h1>Hello world</h1>
+      <style jsx global>{`
+        .homePageStyle {
+          height: 100%;
+          width: 100%;
+          background-color: rgb(0, 0, 0);
+          background-image: url(https://s.pinimg.com/webapp/style/images/bg_multi_case_grid_no_text_white_55_percent-2000ab22.jpg); background-size: cover;
+          padding-bottom: 300px;
+          position: fixed;
+          animation: scrollBgGridLinks 35s linear forwards; background-position: 0px -700px;
+        }
+        .homePage {
+          bottom: 0px;
+          left: 0px;
+          right: 0px;
+          top: 0px;
+          position: absolute;
+          background-color: rgba(0, 0, 0, 0.498039); background-position: initial initial; background-repeat: initial initial;
+        }
+      `}</style>
+    <div className='homePageStyle'></div>
+
+      <div className='homePage'></div>
+
       <Switch>
 
       </Switch>
-      <hr/>
-      <Button color="danger">Danger!</Button>
     </div>
   );
 }
